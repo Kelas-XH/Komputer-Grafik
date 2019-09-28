17fcc762a5a6f038ca0d6783f3eadbbdfdbb328b
aziz66sundanesse <email.abdulaziz16966@gmail.com>
2019-09-28 15:34:24 +0700

    tambah latihan

---
+++ latihan/MyLine/src/MyLine.java
@@ -0,0 +1,7 @@
+import java.awt.Graphics;
+
+public class MyLine extends java.applet.Applet  {
+    public void paint(Graphics g){
+        g.drawLine(25,25,75,75);
+    }
+}
---
+++ latihan/segiEmpat1/src/segiEmpat1.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class segiEmpat1 extends java.applet.Applet {
+    @Override
+    public void paint(Graphics g) {
+//       kiri bawah, kiri atas, panjang , lebar
+//        drawReac blok tanpa isian
+        g. drawRect(20,50,100,60);
+
+//        drawReac blok tanpa isian
+        g. fillRect(150,20,60,100);
+    }
+}
